590b7f2a0b070c48816e5e235e156374a7d0945c
Mattia <mattiafailla@hotmail.it>
2020-07-24 14:39:03 +0200

    Re-writing the home-page

--- tentalog-web/src/App.vue
+++ tentalog-web/src/App.vue
@@ -1,14 +1,128 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <section class="hero is-info is-large has-bg-img">
+      <!--<div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item">
+                <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+              </a>
+              <span class="navbar-burger burger" data-target="navbarMenuHeroB">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div id="navbarMenuHeroB" class="navbar-menu">
+              <div class="navbar-end">
+                <a class="navbar-item is-active">
+                  Home
+                </a>
+                <a class="navbar-item">
+                  Examples
+                </a>
+                <a class="navbar-item">
+                  Documentation
+                </a>
+                <span class="navbar-item">
+                  <a class="button is-info is-inverted">
+                    <span class="icon">
+                      <i class="fab fa-github"></i>
+                    </span>
+                    <span>Read the code</span>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>-->
+      <div class="hero-head">
+      <b-navbar
+      :transparent="true"
+      :spaced="true"
+      >
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="./assets/logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="https://wiki.nextblu.com/">
+                Documentation
+            </b-navbar-item>
+            <b-navbar-item>
+                <b-taglist attached>
+                    <b-tag type="is-dark">TentaLog</b-tag>
+                    <b-tag type="is-info">0.5.1</b-tag>
+                </b-taglist>
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+      </div>
+
+      <div class="hero-body">
+        
+      </div>
+
+      <div class="hero-foot">
+        <nav class="tabs is-boxed is-fullwidth">
+          <div class="container">
+            <ul>
+              <li class="is-active">
+                <a>Overview</a>
+              </li>
+              <li>
+                <a>Example</a>
+              </li>
+              <li>
+                <a>The Web UI</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </section>
+
+
     <router-view/>
+
+
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>TentaLog</strong> by <a href="https://www.nextblu.com">NextBlu</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
+@import '../node_modules/typeface-roboto/index.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,4 +143,20 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+.has-bg-img { 
+  /*background: url('https://images.unsplash.com/photo-1556075798-4825dfaaf498?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1510&q=80')center center; 
+  */background: url('./assets/tentalog-bg-01.png')center center; 
+  background-size:cover; 
+}
+
+.page-title {
+  width: 50% !important;
+  display: inline-block;
+  background: gray;    
+  transform: skewX(-10deg);
+  border-radius: 2px;
+  font-family: Roboto;
+}
+
 </style>
--- tentalog-web/src/views/Home.vue
+++ tentalog-web/src/views/Home.vue
@@ -1,7 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-page">
+    <div class="content">
+      
+
+      <div class="tile is-ancestor mt-5">
+        <div class="tile is-4 is-vertical is-parent">
+          <div class="tile is-child box">
+            <p class="title">Why</p>
+            <p>The answer is simple: we want to make logging a simple, centralized and easy to monitor your application to help you develop simpler, more readable and <code>print()</code>-free programs.</p>
+          </div>
+          <div class="tile is-child box">
+            <p class="title">Future</p>
+            <p>Currently TentaLog is oriented mainly in Python, but we are open to discussions and PR to implement the system in other languages too. Here you can find all the versions of TentaLog: <a>TentaLog | Versions</a></p>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <div class="tile is-child box">
+            <p class="title">How</p>
+            <p>TentaLog is a logging library on top of the default logging library in Python.</p>
+            <p>We managed to integrate a simplier configuration-flow to ensure a quick integration in every app. You can configure your logging with a YAML file. A simple and light weight logging configuration is provided by default. We brought in this library our logging best practices ready to use with the default configuration.</p>
+            <p>
+              The Tentacle is the single unit of TentaLog, managing a single logger. To start logging with tentacle you can follow the <a>quick-start guide on GitHub</a>.
+              This code will use the tentalog default configuration, that contains the configuration of two logging handlers, one for the console and one for a time based file rotation of 15 days. If the "mylogger" logger is not found in configuration, the "root" logger will be used and a warning message will be showed. If the name of the logger is not specified, the "root" logger will be used. If you want to use a custom configuration you can just initialize your Tentacle in a similar way with a 
+              YAML file.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    
+      <div class="columns is-mobile">
+        <div class="column">
+          <section>
+            <b-message title="An important message" type="is-warning" has-icon aria-close-label="Close message">
+            Tentalog is currently in heavy development. You can expect a stable v2.0 release for Aug. 2021          
+            </b-message>
+          </section>
+        </div>
+        <div class="column">
+          <b-notification type="is-info"
+            has-icon
+            aria-close-label="Close notification">
+            Logging is a means of tracking events that happen when some software runs. The software’s developer adds logging calls to their code to indicate that certain events have occurred. An event is described by a descriptive message which can optionally contain variable data (i.e. data that is potentially different for each occurrence of the event). Events also have an importance which the developer ascribes to the event; the importance can also be called the level or severity.
+          </b-notification> 
+        </div>
+      </div>
+  
+
+    
+    </div> 
   </div>
 </template>
 
@@ -16,3 +63,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .home-page {
+    padding: 25px;
+  }
+</style>
