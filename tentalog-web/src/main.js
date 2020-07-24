db8b23c4c800b29b7362d41f036eb96b40bab740
Mattia <mattiafailla@hotmail.it>
2020-07-24 12:34:19 +0200

    Adding buefy.css

--- tentalog-web/src/main.js
+++ tentalog-web/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.use(Buefy)
+
 
 Vue.config.productionTip = false
 
